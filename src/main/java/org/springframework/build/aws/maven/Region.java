0ec7da66792d5151c09857825aa9d7e3e9374760
Florian Rosenberg <f.rosenberg@gmail.com>
2015-10-19 15:04:13 +0200

    Fixed eu-central-1 region

--- src/main/java/org/springframework/build/aws/maven/Region.java
+++ src/main/java/org/springframework/build/aws/maven/Region.java
@@ -17,43 +17,44 @@
 package org.springframework.build.aws.maven;
 
 enum Region {
-    US("US", "s3.amazonaws.com"), //
-    US_WEST_OREGON("us-west-2", "s3-us-west-2.amazonaws.com"), //
-    US_WEST_NORTHERN_CALIFORNIA("us-west-1", "s3-us-west-1.amazonaws.com"), //
-    EU("EU", "s3-eu-west-1.amazonaws.com"), //
-    EU_IRELAND("eu-west-1", "s3-eu-west-1.amazonaws.com"), //
-    GOV_CLOUD("us-gov-west-1", "us-gov-west-1.amazonaws.com"), //
-    ASIA_PACIFIC_SINGAPORE("ap-southeast-1", "s3-ap-southeast-1.amazonaws.com"), //
-    ASIA_PACIFIC_SYDNEY("ap-southeast-2", "s3-ap-southeast-2.amazonaws.com"), //
-    ASIA_PACIFIC_TOKYO("ap-northeast-1", "s3-ap-northeast-1.amazonaws.com"), //
-    SOUTH_AMERICA_SAO_PALO("sa-east-1", "s3-sa-east-1.amazonaws.com");
+		US("US", "s3.amazonaws.com"), //
+		US_WEST_OREGON("us-west-2", "s3-us-west-2.amazonaws.com"), //
+		US_WEST_NORTHERN_CALIFORNIA("us-west-1", "s3-us-west-1.amazonaws.com"), //
+		EU("EU", "s3-eu-west-1.amazonaws.com"), //
+		EU_IRELAND("eu-west-1", "s3-eu-west-1.amazonaws.com"), //
+		EU_FRANKFURT("eu-central-1", "s3-eu-central-1.amazonaws.com"), //
+		GOV_CLOUD("us-gov-west-1", "us-gov-west-1.amazonaws.com"), //
+		ASIA_PACIFIC_SINGAPORE("ap-southeast-1", "s3-ap-southeast-1.amazonaws.com"), //
+		ASIA_PACIFIC_SYDNEY("ap-southeast-2", "s3-ap-southeast-2.amazonaws.com"), //
+		ASIA_PACIFIC_TOKYO("ap-northeast-1", "s3-ap-northeast-1.amazonaws.com"), //
+		SOUTH_AMERICA_SAO_PALO("sa-east-1", "s3-sa-east-1.amazonaws.com");
 
-    private final String locationConstraint;
+		private final String locationConstraint;
 
-    private final String endpoint;
+		private final String endpoint;
 
-    private Region(String locationConstraint, String endpoint) {
-        this.locationConstraint = locationConstraint;
-        this.endpoint = endpoint;
-    }
+		private Region(String locationConstraint, String endpoint) {
+				this.locationConstraint = locationConstraint;
+				this.endpoint = endpoint;
+		}
 
-    String getEndpoint() {
-        return this.endpoint;
-    }
+		String getEndpoint() {
+				return this.endpoint;
+		}
 
-    String getLocationConstraint() {
-        return this.locationConstraint;
-    }
+		String getLocationConstraint() {
+				return this.locationConstraint;
+		}
 
-    static Region fromLocationConstraint(String locationConstraint) {
-        for (Region region : values()) {
-            if (region.getLocationConstraint().equals(locationConstraint)) {
-                return region;
-            }
-        }
+		static Region fromLocationConstraint(String locationConstraint) {
+				for (Region region : values()) {
+						if (region.getLocationConstraint().equals(locationConstraint)) {
+								return region;
+						}
+				}
 
-        throw new IllegalArgumentException(String.format("'%s' is not a valid location constraint",
-                locationConstraint));
-    }
+				throw new IllegalArgumentException(String.format("'%s' is not a valid location constraint",
+								locationConstraint));
+		}
 
 }
